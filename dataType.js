d2dfb1b612ba5db4399d5db5af1aca7fa63464f5
Yogesh <steaverojers07@gmail.com>
2022-07-18 23:56:23 +0530

    new file

---
+++ dataType.js
@@ -0,0 +1,26 @@
+var a = "hello world";
+var b = 10;
+var c = true;
+var d = ["HTML","css","javascript"];
+var e = { first : "yogesh ", last : "upadhyay"};
+var x;
+
+console.log('Data-->'+a);
+console.log('The type of ' +a+ ' is ' +typeof a);
+console.log('----------------------------------------');
+console.log('Data-->'+b);
+console.log('The type of ' +b+ ' is ' +typeof b);
+console.log('----------------------------------------');
+console.log('Data-->'+c);
+console.log('The type of ' +c+ ' is ' +typeof c);
+console.log('----------------------------------------');
+console.log('Data-->'+d);
+console.log('The type of ' +d+ ' is ' +typeof d);
+console.log('----------------------------------------');
+console.log('Data-->'+e);
+console.log('The type of ' +e+ ' is ' +typeof e);
+console.log('----------------------------------------');
+console.log('Data-->'+x);
+console.log('The type of ' +x+ ' is ' +typeof x);
+
+
