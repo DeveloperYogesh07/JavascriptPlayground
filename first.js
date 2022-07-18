b82ce36cf9f798ce4a39940ccbc66c2672cf5188
Yogesh <steaverojers07@gmail.com>
2022-07-18 22:47:35 +0530

    added js file

---
+++ first.js
@@ -0,0 +1,12 @@
+console.log('--------------- first.js ------------------------------------------------------------------\n')
+console.log("First thing First \n Hello world");
+
+console.log('------------------------------------------------------------------------------------------\n')
+
+// function test(a, b){
+//     console.log('My first function', a+10, b);
+// }
+// test(10, 'string')
+const test = (a) => console.log('Arrow function  shik liya mane', a+10);
+
+test(10)
---
+++ second.js
@@ -0,0 +1,12 @@
+console.log('--------------- second.js ------------------------------------------------------------------\n')
+console.log("Welcome to javscript world");
+
+console.log('------------------------------------------------------------------------------------------\n')
+
+// function test(a, b){
+//     console.log('My first function', a+10, b);
+// }
+// test(10, 'string')
+const ts = (a) => console.log('Aaj Kuch tufani karte hai!!!!', a-5);
+
+ts(10)
